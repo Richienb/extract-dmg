a4e540b162aed826c1be5d2fe04f3edba68007e7
Richie Bendall <richiebendall@gmail.com>
2020-02-23 00:48:46 +1300

    feat: Read recursively
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -4,7 +4,12 @@ const extractDmg = require(".")
 test("main", async (t) => {
 	if (process.platform === "darwin") {
 		const contents = await extractDmg("fixtures/vlc-3.0.8.dmg")
-		t.deepEqual(contents, ["vlc-3.0.8"])
+		t.deepEqual(contents, [
+			'.VolumeIcon.icns',
+			'.background.tiff',
+			'Applications',
+			'VLC.app',
+		])
 	} else {
 		t.pass()
 	}
